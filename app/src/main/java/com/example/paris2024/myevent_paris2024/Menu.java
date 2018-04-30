306ca995cf959ad4342bffab6deec24d4a05db8e
sergio582 <33252914+sergio582@users.noreply.github.com>
2018-04-30 10:09:19 +0200

    modif lien connexion reussi

---
+++ app/src/main/java/com/example/paris2024/myevent_paris2024/Menu.java
@@ -0,0 +1,14 @@
+package com.example.paris2024.myevent_paris2024;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
